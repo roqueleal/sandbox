df9d8cedfbe79202a89c4549a072b1a1b55f46a1
Roque Leal <32916827+roqueleal@users.noreply.github.com>
2020-10-19 07:35:52 -0500

    de
    de

---
+++ aop/config.js
@@ -0,0 +1,38 @@
+
+const config = {
+    style: "mapbox://styles/vistcomunicacion/ckf5qsnnj2h8f19qqdim9r8m1",
+    accessToken: "pk.eyJ1IjoidmlzdGNvbXVuaWNhY2lvbiIsImEiOiJja2Nyc3ZiYzQxaTJ4MnFzNXBpMG5iZno2In0.9bPy87fQMJpOmV2sJ_AYWQ",
+    CSV: "./finale.csv",
+    center: [-80.325780, -2.477416], //Lng, Lat
+    zoom: 8, //Default zoom
+    title: "Geovisor Camaronero",
+    description: "Entorno de prueba",
+    sideBarInfo: ["NOMBRE"],
+    popupNom: ["NOMBRE"],
+    popupDescrip:["Suprf-Ha"],
+    popupWeb:["INGName"],
+    filters: [
+        {
+            type: "checkbox",
+            title: "Thematique : ",
+            columnHeader: "produit",
+            listItems: ["Munster","Touraine"]
+        },
+        {
+            type: "dropdown",
+            title: "region",
+            columnHeader: "region",
+            listItems: [
+              'Grand Est',
+                'Normandie'
+            ]
+        },
+        {
+            type: "checkbox",
+            title: "Type",
+            columnHeader: "type",
+            listItems: ["Fromages", "Eaux"]
+        }
+    ]
+
+};
